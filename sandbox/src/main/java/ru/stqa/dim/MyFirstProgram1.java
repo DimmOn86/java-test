cb5786d1d15ff8f32fce83aafdf6fd028d2b35ff
DimmOn <dimmon-86@mail.ru>
2018-04-10 21:56:43 +0500

    созданы функции

--- sandbox/src/main/java/ru/stqa/dim/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/dim/MyFirstProgram1.java
@@ -4,7 +4,26 @@ public class MyFirstProgram1 {
 
 
     public static void main(String[] args) {
+
+        hello();
+        double l = 6;
+        System.out.println("Площадь квадрата со сторойной " + l + " = " + area(l));
+        double a = 4;
+        double b =6;
+        System.out.println("Произведение чисел " + a + " + " + b + " = " + area(a,b));
+
+    }
+
+    public static void hello(){
         System.out.println("Hello, world!");
+    }
+
+    public static double area(double l){
+      return l * l;
+    }
+
+    public static double area(double a, double b){
+      return a * b;
     }
 
 }

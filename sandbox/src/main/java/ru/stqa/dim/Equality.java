61a09ddeec357d206ba863cfc26a21f671011b3d
DimmOn <dimmon-86@mail.ru>
2018-04-23 22:03:17 +0500

    пример со сравнениями переменных, значения по умолчанию для текстовых полей

---
+++ sandbox/src/main/java/ru/stqa/dim/Equality.java
@@ -0,0 +1,12 @@
+package ru.stqa.dim;
+
+public class Equality {
+
+  public static void main (String[] args){
+    String s1 = "FireFox";
+    String s2 = "FireFox";
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+  }
+}

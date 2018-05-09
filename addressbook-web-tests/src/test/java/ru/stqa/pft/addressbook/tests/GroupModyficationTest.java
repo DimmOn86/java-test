3c09924625367ceec01c2065f4778e9739fd0acc
DimmOn <dimmon-86@mail.ru>
2018-05-09 22:51:31 +0500

    изменение переменных before и after на списки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModyficationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModyficationTest.java
@@ -4,22 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModyficationTest extends TestBase {
 
   @Test
   public void testGroupModyfication(){
     app.getNavigationHelper().goToGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if(! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().CreateGroup(new GroupData("FirstGroup", "Test 1", "Test 2"));
     }
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("FirstGroup", "test1", "Test2"));
     app.getGroupHelper().submitModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
 
 
   }
